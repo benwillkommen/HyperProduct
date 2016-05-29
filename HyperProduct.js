cfec062bf8c4ff6d8e399c434836f6011829a3a7
ben willkommen <benwillkommen@gmail.com>
2016-05-29 08:46:04 -0500

    just getting boilerplate set up

---
+++ HyperProduct.js
@@ -0,0 +1,9 @@
+var HyperProduct = function(variants, dimensions){
+	var sortedDimensions = dimensions.sort();
+	var variantHashTable = {};
+	for (var i = 0; i < variants.length; i++){
+		variantHashTable
+	}
+};
+
+module.exports = HyperProduct;
