38866e19cfafd51c8f1d6516668042dc064c6051
ben willkommen <benwillkommen@gmail.com>
2016-05-29 11:15:56 -0500

    jest tests aren't running, but getSelectableDimensions and getVariant seem to be working from my manual tests

--- HyperProduct.js
+++ HyperProduct.js
@@ -1,10 +1,84 @@
 'use strict'
 
 var HyperProduct = function(variants, dimensions){
+	var self = this;
+
+	//TODO: make dimensions an object with keys that are dimension names, and values
+	//that are configuration, e.g. default dimension values
 	var sortedDimensions = dimensions.sort();
 	var variantHashTable = {};
+	var selectedDimensions = {};
+
+	var formatKey = function(variant){
+		var dimensionValues = []
+		for (var i = 0; i < sortedDimensions.length; i++){
+			dimensionValues.push(variant[sortedDimensions[i]])
+		}
+		return dimensionValues.join(":");
+	}
+
 	for (var i = 0; i < variants.length; i++){
-		//variantHashTable
+		let variant = variants[i];
+		variantHashTable[formatKey(variant)] = variant;
+	}
+
+	self.getVariant = function(variantDimensions) {
+		if (Object.keys(variantDimensions).length < sortedDimensions.length){
+			return undefined;
+		}
+		return variantHashTable[formatKey(variantDimensions)];
+	}
+
+	self.getSelectableDimensions = function(){
+		let selectableDimensions = {};
+		var dimensionsToReturn = sortedDimensions.filter(function(val){
+			return typeof selectedDimensions[val] === "undefined";
+		});
+		
+		for (var i = 0; i < dimensionsToReturn.length; i++){
+			selectableDimensions[dimensionsToReturn[i]] = new Set();
+		}
+
+		for (var i = 0; i < variants.length; i++){
+			
+
+			var variant = variants[i];
+			var dimensionCombinationExists = true;
+
+
+			for (let d in selectedDimensions){
+
+
+				if (typeof d === "undefined"){
+					//no selection has been made for this dimension
+					continue;
+				}	
+
+				if(variant[d] === selectedDimensions[d]){
+					//current variant could have dimension values matching selected dimensions
+					continue;
+				}
+
+				//if we get here, the variant has a dimension that does not match the selected dimesions
+				dimensionCombinationExists = false;
+				break;
+			}
+
+			if (dimensionCombinationExists){
+				//add dimensions of this variant to the selectable dimensions ONLY for dimensions that
+				//are not currently selected				
+				for (let d in selectableDimensions){
+					selectableDimensions[d].add(variant[d]);
+				}
+
+			}
+		}
+		return selectableDimensions;
+
+	}
+
+	self.setDimension = function(key, value){
+		selectedDimensions[key] = value;
 	}
 };
 
--- __tests__/hyperproduct-test.js
+++ __tests__/hyperproduct-test.js
@@ -1,10 +1,45 @@
 'use strict'
-
+//console.log("pls");
 jest.unmock("../hyperproduct");
+jest.autoMockOff();
 
-describe("HyperProduct", () => {
-	it("has a fake test"), () => {
+
+
+describe("HyperProduct.getVariant", () => {
+	it("returns a variant given dimensions"), () => {
 		var HyperProduct = require("../hyperproduct");
+		var dimensions = ["size", "color"];
+		var variants = 	[
+							{"size": "L", "color": "white", "id": "123"},
+							{"size": "M", "color": "white", "id": "456"},
+							{"size": "L", "color": "black", "id": "789"}
+						];
+		console.log("pls")
+		var hyperProduct = new HyperProduct(variants, dimensions);
+
+		var variant = hyperProduct.getVariant({"size":"M", "color": "white"});
+
+		//wtf jest?
+		expect(variant).toBe(false);
+	};
+	
+});
+
+describe("HyperProduct.getSelectableDimensions", () => {
+
+	it("returns selectable dimensions"), () => {
+		var HyperProduct = require("../hyperproduct");
+		var dimensions = ["size", "color"];
+		var variants = 	[
+							{"size": "L", "color": "white", "id": "123"},
+							{"size": "M", "color": "white", "id": "456"},
+							{"size": "L", "color": "black", "id": "789"}
+						];
+		debugger;
+		var hyperProduct = new HyperProduct(variants, dimensions);
+
+		var selectableDimensions = hyperProduct.getSelectableDimensions();
+		console.log(selectableDimensions);
 		expect(true).toBe(true);
 	}
 });
